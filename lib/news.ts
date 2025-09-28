42b54319190f68e09373ea5399a9ee2f131e0eeb
damhian <david.mayshiogie@protonmail.com>
2025-09-28 22:58:14 +0700

    add some UI to dashboard continue to create select page

---
+++ lib/news.ts
@@ -0,0 +1,35 @@
+import { err } from "inngest/types";
+import { url } from "inspector";
+
+export async function fetchArticles(
+  categories: string[],
+): Promise<Array<{ title: string; url: string; category: string }>> {
+  const since = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOString(); // Articles from the last 7 days
+
+  const promises = categories.map(async (category) => {
+    try {
+      const response = await fetch(
+        `https://newsapi.org/v2/everything?q=${encodeURIComponent(
+          category,
+        )}&from=${since}&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}`,
+      );
+      if (!response.ok) {
+        console.error("Failed fetching from this category", category);
+        return [];
+      }
+
+      const data = await response.json();
+      return data.articles.slice(0, 5).map((article: any) => ({
+        title: article.title || "No title",
+        url: article.url || "#",
+        description: article.description || "No description available",
+      }));
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  });
+
+  const result = await Promise.all(promises);
+  return result.flat();
+}
